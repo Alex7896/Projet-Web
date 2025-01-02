c4c0f1f64af6525731d374813e4d9d79a914da82
housni2003 <rakotoari.housni@gmail.com>
2025-01-02 12:56:38 +0100

    render stagiare

--- public/stagiaire.php
+++ public/stagiaire.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../vendor/autoload.php'; // Inclure le loader de Twig
 session_start();
 
 if (!isset($_SESSION['user'])) {
@@ -11,6 +12,15 @@ if ($_SESSION['user']['role'] !== 'enseignant') {
     echo "Accès refusé. Cette page est réservée aux enseignants.";
     exit;
 }
+
+require_once '../vendor/autoload.php';
+
+
+$loader = new \Twig\Loader\FilesystemLoader('../templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+]);
+
 
 echo $twig->render('stagiaire.twig', [
     'nom' => 'Utilisateur',
