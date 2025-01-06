44c445c7924c7f65a2f84c9c315808b70ebf9f3d
housni2003 <rakotoari.housni@gmail.com>
2025-01-06 20:02:29 +0100

    navbar et aide.php

---
+++ public/aide.php
@@ -0,0 +1,12 @@
+<?php
+require_once '../vendor/autoload.php';
+
+require_once 'connect.php'; // Inclure la connexion à la base de données
+
+
+// Configuration de Twig
+$loader = new \Twig\Loader\FilesystemLoader('../templates');
+$twig = new \Twig\Environment($loader);
+
+// Rendre le fichier Twig
+echo $twig->render('aide.twig');
